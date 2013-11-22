61411a6b598963ed24f7063b89513ea83b47ea08
Veeera <Veeera@Veeera-PC>
2013-11-21 17:53:28 -0800

    Adding new class files - forgot to add in the previous commit

---
+++ CS576_Project/src/ErrorData.java
@@ -0,0 +1,12 @@
+
+public class ErrorData {
+	public float hError;
+	public float yError;
+	public int startIndex;
+	
+	ErrorData() {
+		hError = 0;
+		yError = 0;
+		startIndex = 0;
+	}
+}
---
+++ CS576_Project/src/MatchParameters.java
@@ -0,0 +1,20 @@
+
+public class MatchParameters {
+	Integer h[];
+	Integer y[];
+	
+	public MatchParameters() {
+		int i = 0;
+		
+		h = new Integer[Constants.H_QUANTIZATION_FACTOR];
+		y = new Integer[Constants.Y_QUANTIZATION_FACTOR];
+		
+		for(i = 0; i < Constants.H_QUANTIZATION_FACTOR; i++) {
+			h[i] = 0;
+		}
+		
+		for(i = 0; i < Constants.Y_QUANTIZATION_FACTOR; i++) {
+			y[i] = 0;
+		}
+	}
+}
