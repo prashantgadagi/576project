f62be6d6eefa9e0392bf2ade48a4506717c9bdb1
Prashant <Prashant@Prashant-PC>
2013-12-08 14:48:11 -0800

    Fixed the audio lag issue again! This time for good!

--- CS576_Project/src/SliderMotionListener.java
+++ CS576_Project/src/SliderMotionListener.java
@@ -6,24 +6,24 @@ public class SliderMotionListener extends MouseAdapter {
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		if(UI.play != null) {
-			int frameLength = Constants.WIDTH * Constants.HEIGHT * 3;
-			long pos = frameLength * UI.slider.getValue();
+			long pos = Constants.WIDTH * Constants.HEIGHT * 3 * UI.slider.getValue();
+			int audioPos = PlaySound.EXTERNAL_BUFFER_SIZE * UI.slider.getValue();
 			
-			//int audioPos = (int) ((UI.audioThread.playSound.frameLength * 2 ) * ((1.0 * UI.slider.getValue()) / UI.slider.getMaximum()));
-			int audioPos = (int) (PlaySound.EXTERNAL_BUFFER_SIZE * UI.slider.getValue());
 			try {
+				//Skipping video
 				UI.videoThread.fileAccess.seek(pos);
 				UI.videoThread.totalRead = pos;
 				
+				//Skipping audio
 				UI.audioThread.playSound.audioInputStream.reset();
 				UI.audioThread.playSound.dataLine.flush();
-				//UI.audioThread.playSound.dataLine.stop();
-				//UI.audioThread.playSound.dataLine.start();
-				UI.audioThread.playSound.audioInputStream.skip(audioPos);				
+				while (audioPos > 0) {
+					UI.audioThread.playSound.audioInputStream.skip(audioPos > 65536 ? 65536 : audioPos);
+					audioPos -= 65536;
+				}		
 			} catch (IOException e) {
 				System.out.println(e.getMessage());
 			}
-			
 			UI.play.mouseClicked(null);
 		}
 	}
